ec24995cec77d7657682571303581ad12e60e543
hama <21331104th@tama.ac.jp>
2021-03-10 17:30:28 +0900

    最初のコミット

---
+++ Xib_TODO_LIST/XibTableView.swift
@@ -0,0 +1,20 @@
+//
+//  XibTableView.swift
+//  Xib_TODO_LIST
+//
+//  Created by 濱貴大 on 2021/03/06.
+//
+
+import UIKit
+
+class XibTableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
