12111a2d938973273717530951fe13712ab01600
hama <21331104th@tama.ac.jp>
2021-03-12 22:08:56 +0900

    MVC追加

---
+++ Xib_TODO_LIST/Controller/XibTableView.swift
@@ -0,0 +1,29 @@
+//
+//  XibTableView.swift
+//  Xib_TODO_LIST
+//
+//  Created by 濱貴大 on 2021/03/06.
+//
+
+import UIKit
+
+class XibTableView: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    //最初からあるコード
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    //最初からあるコード
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
